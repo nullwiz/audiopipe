28f0aab61ce819119844d3e2d0c0a34d07cf65a1
Marco Iurman <marco@telescopepartners.com>
2025-04-28 12:18:01 -0300

    test

--- conftest.py
+++ conftest.py
@@ -3,40 +3,8 @@ import os
 from pathlib import Path
 
 
-def pytest_addoption(parser):
-    parser.addoption(
-        "--runslow", action="store_true", default=False, help="run slow tests"
-    )
-    parser.addoption(
-        "--integration",
-        action="store_true",
-        default=False,
-        help="run integration tests",
-    )
-
-
-def pytest_configure(config):
-    config.addinivalue_line("markers", "slow: mark test as slow to run")
-    config.addinivalue_line("markers", "integration: mark test as an integration test")
-
-
-def pytest_collection_modifyitems(config, items):
-    # Handle slow tests
-    if not config.getoption("--runslow"):
-        skip_slow = pytest.mark.skip(reason="need --runslow option to run")
-        for item in items:
-            if "slow" in item.keywords:
-                item.add_marker(skip_slow)
-
-    # Handle integration tests
-    if not config.getoption("--integration"):
-        skip_integration = pytest.mark.skip(reason="need --integration option to run")
-        for item in items:
-            if (
-                "integration" in item.keywords
-                and "test_integration" in item.parent.name
-            ):
-                item.add_marker(skip_integration)
+# Note: Main pytest options are defined in test/conftest.py
+# This file only contains project-wide fixtures and setup
 
 
 @pytest.fixture(scope="session", autouse=True)
